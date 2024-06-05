3e1046c23b7b8dac3bfbf96da0fb4be00e200f20
Sofiya <925038623sss@gmail.com>
2024-06-05 12:13:41 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 21_300;
+        int oneMiles = 20; // итог на экране 1_065
+        System.out.println(price / oneMiles);
+
+    }
+}
